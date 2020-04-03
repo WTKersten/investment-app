19cd00559fb683b4b2adbadeda90f57fc85c1cd8
Willem Kersten <willemkersten@gmail.com>
2020-04-03 11:54:01 +0200

    export theme

--- src/containers/theme.js
+++ src/containers/theme.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-const theme = {
+export const theme = {
     colors: {
         light: '#FFFDF9',
         dark: '#36313D',
